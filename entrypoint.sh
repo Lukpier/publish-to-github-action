e5318c3e44169c326ef644c33a54d44c51db143e
Luca Pierri <lukepier95@gmail.com>
2022-01-07 20:24:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,8 @@ remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSIT
 git config http.sslVerify false
 git config user.name "Automated Publisher"
 git config user.email "actions@users.noreply.github.com"
-git config --global alias.hist "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short"git remote add publisher "${remote_repo}"
+git config --global alias.hist "log --pretty=format:'%C(yellow)[%ad]%C(reset) %C(green)[%h]%C(reset) | %C(red)%s %C(bold red){{%an}}%C(reset) %C(blue)%d%C(reset)' --graph --date=short"
+git remote add publisher "${remote_repo}"
 git show-ref # useful for debugging
 git branch --verbose
 
